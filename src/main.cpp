b449d90b3b6dbffe1129f092095f1dfc217da90c
Avneet Singh <singhab2024@gmail.com>
2025-10-12 15:27:44 -0700

    created request body and http req

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,20 @@ int main()
     
     // initializes the client wrapper with the read in API key from my local .env
     openai::start();
+    std::cout << "OpenAI Client Successfully Created.\n\n";
 
-    std::cout << "OpenAI Client Successfully Created.\n";
+    // build the request body in json
+    openai::Json req {
+      {"model", "gpt-5"},
+      {"messages", openai::Json::array({
+          {{"role", "system"}, {"content", "You just respond with just 'TEST' for now."}},
+          {{"role", "user"}, {"content", "Test Request"}}
+          })}
+    };
+
+    // makes the HTTP call through client wrapper
+    openai::Json res = openai::chat().create(req);
+
 
   } catch (const std::exception& e) {
     std::cerr << "Error: " << e.what() << '\n';
